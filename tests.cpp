2a162aa5ce08f69a8610d68855977dc7d771a043
iceicebabyaf <max.nonkin@bk.ru>
2025-03-12 20:42:06 +0700

    2.4

--- tests.cpp
+++ tests.cpp
@@ -49,3 +49,18 @@ TEST(CandleTest, FullSize_Negative) {
 }
 
 
+//      2.4
+TEST(CandleTest, BodySize_Into){
+    Candle candle(100.0, 150.0, 90.0, 120.0);
+    EXPECT_GT(candle.body_size(), -1);
+}
+TEST(CandleTest, BodySize_EQ){
+    Candle candle(180.0, 150.0, 90.0, 120.0);
+    EXPECT_EQ(candle.body_size(), 60.0);
+}
+TEST(CandleTest, BodySize_Negative) {
+    Candle candle(100.0, 150.0, 250.0, 200.0);
+    EXPECT_EQ(candle.body_size(), 100.0);
+}
+
+
