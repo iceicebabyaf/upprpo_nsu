da1e7fe5602d7d8dc04d365c6ca0126998ef71cd
iceicebabyaf <max.nonkin@bk.ru>
2025-03-12 20:41:32 +0700

    2.2

--- tests.cpp
+++ tests.cpp
@@ -19,4 +19,18 @@ TEST(CandleTest, BodyContains_OutsideBody) {
     EXPECT_EQ(candle.body_contains(130.0), 0);
 }
 
+//      2.2
+TEST(CandleTest, Cointains_High){
+    Candle candle(100.0, 150.0, 90.0, 120.0);
+    EXPECT_EQ(candle.contains(150.0), 1);
+}
+TEST(CandleTest, Contains_Mid){
+    Candle candle(100.0, 150.0, 90.0, 120.0);
+    EXPECT_EQ(candle.contains(100.0), 1);
+}
+TEST(CandleTest, Contains_Out){
+    Candle candle(100.0, 150.0, 90.0, 120.0);
+    EXPECT_EQ(candle.contains(89.99999999), 0);
+}
+
 
